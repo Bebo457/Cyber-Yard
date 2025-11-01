41dcc7b6b955b5b8e146ca1994fbf2e4cb501a18
Siedmiu <grzesiapoczta@gmail.com>
2025-11-01 15:32:23 +0100

    oopsie

--- program/src/PlayerController.cpp
+++ program/src/PlayerController.cpp
@@ -85,8 +85,6 @@ MoveDecision AIPlayerController::CalculateBestMove(
         return decision;
     }
 
-    }
-
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, static_cast<int>(vec_PossibleMoves.size()) - 1);
